6d4b1bfc7ba4ae0a8dedad75e2fdfba10f31c49f
f1sh3r <h4ck3df1sh@proton.me>
2025-12-30 16:13:23 +0100

    Replace py_modules with scripts in setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,9 @@ setup(
     version='1.0.0',
     description='PoC to elicit machine account authentication via MS-EFSRPC functions',
     author='GILLES Lionel aka topotam',
-    py_modules=['PetitPotam'],
+    scripts=['PetitPotam.py'],
     install_requires=[
         'impacket',
     ],
-    entry_points={
-        'console_scripts': [
-            'PetitPotam.py=PetitPotam:main',
-        ],
-    },
     python_requires='>=3.6',
 )
